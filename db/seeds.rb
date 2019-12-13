5412b5367203c3b737fc6e0b708a780937147bc6
App Academy Student <email@email.com>
2019-12-13 11:34:32 -0500

    disabled active storage while working on heroku issues

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,24 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
+User.destroy_all
 
 User.create!([{first_name: "Jon", last_name: "Snow", email: "JonSnow@starks.com", password: "gameofthrones", birth_date: DateTime.new(1990-05-26)},
              {first_name: "Ned", last_name: "Stark", email: "NedStark@starks.com", password: "gameofthrones", birth_date: DateTime.new(1965-02-16)},
              {first_name: "Jamie", last_name: "Lannister", email: "JamieLannister@lannisters.com", password: "gameofthrones", birth_date: DateTime.new(1980-06-12)},
              {first_name: "Sansa", last_name: "Stark", email: "SansaStark@starks.com", password: "gameofthrones", birth_date: DateTime.new(1992-5-26)}])
 
-# user1 = User.first
-# user2 = User.all[1]
-# user3 = User.all[2]
-# user4 = User.all[3]
+user1 = User.all[0];
+user2 = User.all[1];
+user3 = User.all[2];
+user4 = User.all[3];
 
-# file1 = File.open('app/assets/images/jon-snow.jpg')
-# file2 = File.open('app/assets/images/ned-stark.jpg')
-# file3 = File.open('app/assets/images/jamie-lannister.jpg')
-# file4 = File.open('app/assets/images/sansa-stark.jpg')
+file1 = File.open('app/assets/images/jon-snow.jpg')
+file2 = File.open('app/assets/images/ned-stark.jpg')
+file3 = File.open('app/assets/images/jamie-lannister.jpg')
+file4 = File.open('app/assets/images/sansa-stark.jpg')
 
-# user1.photo.attach(file1)
-# user2.photo.attach(file2)
-# user3.photo.attach(file3)
-# user4.photo.attach(file4)
+user1.profile_photo.attach(io:file1, filename: 'jon-snow.jpg')
+user2.profile_photo.attach(io:file2, filename: 'ned-stark.jpg')
+user3.profile_photo.attach(io:file3, filename: 'jamie-lannister.jpg')
+user4.profile_photo.attach(io:file4, filename: 'sansa-stark.jpg')
