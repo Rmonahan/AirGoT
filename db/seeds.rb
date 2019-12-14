25b7ef02fd715d38b93d7ca26b2e4b8db6878ef7
Rmonahan <monahan.62@osu.edu>
2019-12-14 14:35:17 -0500

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+
+
 User.destroy_all
 
 User.create!([{first_name: "Jon", last_name: "Snow", email: "JonSnow@starks.com", password: "gameofthrones", birth_date: DateTime.new(1990-05-26)},
@@ -17,16 +13,17 @@ user2 = User.all[1];
 user3 = User.all[2];
 user4 = User.all[3];
 
-file1 = File.open('app/assets/images/jon-snow.jpg')
+file1 = open('https://s3.amazonaws.com/airgot-dev/images/jon-snow.jpg')
 file2 = File.open('app/assets/images/ned-stark.jpg')
 file3 = File.open('app/assets/images/jamie-lannister.jpg')
 file4 = File.open('app/assets/images/sansa-stark.jpg')
+puts(file1)
 
 
-user1.profile_photo.attach(io:file1, filename: 'jon-snow.jpg')
-user2.profile_photo.attach(io:file2, filename: 'ned-stark.jpg')
-user3.profile_photo.attach(io:file3, filename: 'jamie-lannister.jpg')
-user4.profile_photo.attach(io:file4, filename: 'sansa-stark.jpg')
+user1.profile_photo.attach(io: file1, filename: 'jon-snow.jpg', content_type: "image/jpg")
+user2.profile_photo.attach(io: file2, filename: 'ned-stark.jpg')
+user3.profile_photo.attach(io: file3, filename: 'jamie-lannister.jpg')
+user4.profile_photo.attach(io: file4, filename: 'sansa-stark.jpg')
 
 
 
