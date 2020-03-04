4e989e99f637b4c73912c2303d037e890adb695b
Rmonahan <monahan.62@osu.edu>
2020-03-04 17:55:40 -0500

    took methods off the window

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDom from "react-dom";
 import configureStore from "./store/store";
 import Root from "./components/root";
-import {login, logout, signup} from "./actions/session";
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
@@ -20,11 +19,6 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store = configureStore();
   }
- 
-  window.login = login;
-  window.logout = logout;
-  window.signup = signup;
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  
   ReactDom.render(<Root store={store} />, root);
 });
